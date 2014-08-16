f52099515a25573b40bcead9f0be93f057a875ba
Frossie Economou <frossie@lsst.org>
2014-08-16 13:49:38 -0700

    more dumb tests

--- foo.py
+++ foo.py
@@ -3,3 +3,11 @@ Controol
 Controol(foo)
 bar.Controol
 bar.Controol(foo)
+
+bar.Controol(state.env)
+bar.Controol(state)
+
+
+
+control = bar.Controol(state.env, ignoreList=ignoreList, args=args, verbose=True)
+controol = bar.Controol(state.env, ignoreList=ignoreList, args=args, verbose=True)
